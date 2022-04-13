ed1b0771de36eef18c84fc83c969ff8b041c0c11
czbijian <czbijian@outlook.com>
2022-04-13 09:37:17 +0800

    add project

---
+++ WorkSpace/JSD1604/src/day01/HelloWorld.java
@@ -0,0 +1,15 @@
+package day01;
+
+public class HelloWorld{//声明类HelloWorld
+	//主方法，为程序的入口
+	//程序的执行从main开始，main结束则程序结束
+	public static void main(String[] args) {
+    		//输出HelloWorld
+    		//1.严格区分大小写
+    		//2.符号必须是英文模式的
+    		//3.每句话的结尾要用;结尾
+    		System.out.print("Hello");
+    		System.out.println("World");
+    		System.out.println("今天天气不错，心情好");
+	}	
+}
